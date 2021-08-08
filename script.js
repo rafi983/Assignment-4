73da5b35f5948e06648ac77ba95a390f7f7d9536
rafi983 <rafiirfan211@gmail.com>
2021-08-09 00:18:30 +0600

    problem-3-added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // ========================Problem - 1============================== //
-/*
+
 function seerToMon(seer) {
   if (typeof seer != "number") {
     return "Please give how many seer u want";
@@ -17,12 +17,11 @@ const wheatMon2 = seerToMon("wheat floor");
 console.log(riceMon);
 console.log(wheatMon);
 console.log(wheatMon2);
-*/
 
 // ===============================xxx======================================== //
 
 // =================================Problem - 2====================================== //
-/*
+
 function totalSales(shirts, pants, shoes) {
   if (typeof (shirts && pants && shoes) != "number") {
     return "Give me a number of how many u want to buy for Eid shopping 😀😀";
@@ -49,7 +48,52 @@ let sumSales2 = totalSales(2, 3, 5);
 console.log(sumSales);
 console.log(sumSales1);
 console.log(sumSales2);
-*/
+
 // =====================================xxx================================== //
 
 // ==============================Problem - 3============================== //
+
+function deliveryCost(shirts) {
+  const firstHundredShirtsPerPrice = 100;
+  const secondHundredShirtsPerPrice = 80;
+  const restShirtsPerPrice = 50;
+
+  if (typeof shirts != "number") {
+    return "please give me a number of shirts";
+  } else if (shirts < 0) {
+    return "please give a positive number";
+  } else {
+    if (shirts <= 100) {
+      const firstHundredShirtsCosts = shirts * firstHundredShirtsPerPrice;
+      return firstHundredShirtsCosts;
+    } else if (shirts <= 200) {
+      const firstHundredShirtsCosts = 100 * firstHundredShirtsPerPrice;
+      const restShirts = shirts - 100;
+      const secondHundredShirtsCosts = restShirts * secondHundredShirtsPerPrice;
+      const totalShirtsDeliveryCosts =
+        firstHundredShirtsCosts + secondHundredShirtsCosts;
+      return totalShirtsDeliveryCosts;
+    } else {
+      const firstHundredShirtsCosts = 100 * firstHundredShirtsPerPrice;
+      const secondHundredShirtsCosts = 100 * secondHundredShirtsPerPrice;
+      const restShirts = shirts - 200;
+      const restShirtsCosts = restShirts * restShirtsPerPrice;
+      const totalShirtsDeliveryCosts =
+        firstHundredShirtsCosts + secondHundredShirtsCosts + restShirtsCosts;
+
+      return totalShirtsDeliveryCosts;
+    }
+  }
+}
+const shirtsDelivery = deliveryCost(120);
+const shirtsDelivery1 = deliveryCost(250);
+const shirtsDelivery2 = deliveryCost(330);
+const shirtsDelivery3 = deliveryCost(-330);
+const shirtsDelivery4 = deliveryCost("shirts");
+console.log(shirtsDelivery);
+console.log(shirtsDelivery1);
+console.log(shirtsDelivery2);
+console.log(shirtsDelivery3);
+console.log(shirtsDelivery4);
+
+// ========================================xxx==================================== //
